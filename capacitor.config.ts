7e4676da343090b9ca6ac4d5d255926093b632e8
aDarkMaker <2261265112@qq.com>
2025-11-11 18:22:39 +0800

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'huaxiaoke.com',
+  appName: 'hxkterminal-mobile',
+  webDir: 'dist'
+};
+
+export default config;
